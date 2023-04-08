5833f9ebf03b5c7cf65823c78a4263ab06220d48
sameermd16 <sameerthemc@gmail.com>
2023-04-08 17:48:30 +0530

    nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,6 +17,40 @@ const Header=  (props) =>  {
                     <img src="/images/search-icon.svg"/>
                 </SearchIcon>
             </Search>
+            <Nav>
+                <NavListWrap>
+                    <NavList className="active">
+                        <a>
+                            <img src="/images/nav-home.svg"/>
+                            <span>Home</span>
+                        </a>
+                    </NavList>
+                    <NavList>
+                        <a>
+                            <img src="/images/nav-network.svg"/>
+                            <span>My Network</span>
+                        </a>
+                    </NavList>
+                    <NavList>
+                        <a>
+                            <img src="/images/nav-jobs.svg"/>
+                            <span>Jobs</span>
+                        </a>
+                    </NavList>
+                    <NavList>
+                        <a>
+                            <img src="/images/nav-messaging.svg"/>
+                            <span>Messaging</span>
+                        </a>
+                    </NavList>
+                    <NavList>
+                        <a>
+                            <img src="/images/nav-notifications.svg"/>
+                            <span>Notifications</span>
+                        </a>
+                    </NavList>
+                </NavListWrap>
+            </Nav>
         </Content>
     </Container>
   )
@@ -48,6 +82,7 @@ const Logo = styled.span`
 `;
 
 const Search = styled.div`
+    /* border: 1px solid black; */
     position: relative;
     opacity: 1;
     flex-grow: 1;
@@ -82,7 +117,95 @@ const SearchIcon = styled.div`
     justify-content: center;
     align-items: center;
     transition: background-color 0.15s;
+    margin: 0;
 `;
+
+const Nav = styled.nav`
+    /* border: 1px solid red; */
+    margin-left: auto;
+    display: block;
+    @media (max-width: 768px) {
+        position: fixed;
+        left: 0;
+        bottom: 0;
+        background: white;
+        width: 100%;
+    }
+`;
+
+const NavListWrap = styled.ul`
+    display: flex;
+    flex-wrap: nowrap;
+    list-style-type: none;
+    padding-top: 10px;
+
+    .active {
+        span::after {
+            content: '';
+            transform: scaleX(1);
+            border-bottom: 2px solid var(--white, #fff);
+            bottom: 0;
+            left: 0;
+            position: absolute;
+            transition: transform .2s ease-in-out;
+            width: 100%;
+            border-color: rgba(0, 0, 0, 0.9);
+        }
+    }
+`;
+
+const NavList = styled.li`
+    display: flex;
+    align-items: center;
+  
+    a {
+        align-items: center;
+        background: transparent;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        font-size: 12px;
+        font-weight: 400;
+        line-height: 1.5;
+        min-height: 42px;
+        min-width: 80px;
+        position: relative;
+        text-decoration: none;
+        span {
+        color: rgba(0, 0, 0, 0.6);
+        display: flex;
+        align-items: center;
+        }
+        @media (max-width: 768px) {
+            min-width: 70px;
+        }
+    }
+    &:hover, &:active {
+        a {
+            span {
+                color: rgba(0, 0, 0, 0.9);
+            }
+        }
+    }
+`;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
