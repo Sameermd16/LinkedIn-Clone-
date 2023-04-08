a9a0d90fa3d61035175937942b7b02ad15f36af9
sameermd16 <sameerthemc@gmail.com>
2023-04-08 08:12:37 +0530

    search icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,6 +9,14 @@ const Header=  (props) =>  {
                     <img src="/images/home-logo.svg"/>
                 </a>
             </Logo>
+            <Search>
+                <div>
+                    <input type="text" placeholder="Search"/>
+                </div>
+                <SearchIcon>
+                    <img src="/images/search-icon.svg"/>
+                </SearchIcon>
+            </Search>
         </Content>
     </Container>
   )
@@ -38,5 +46,44 @@ const Logo = styled.span`
     margin-right: 8px;
     font-size: 0px;
 `;
+
+const Search = styled.div`
+    position: relative;
+    opacity: 1;
+    flex-grow: 1;
+    margin-left: 5px;
+    & > div {
+        max-width: 280px;
+        input {
+            border: none;
+            box-shadow: none;
+            background-color: #eef3f8;
+            border-radius: 2px;
+            width: 218px;
+            height: 34px;
+            padding: 0 8px 0 40px;
+            line-height: 1.75;
+            font-weight: 400;
+            font-size: 14px;
+            border-color: #dce6f1;
+            vertical-align: text-top;
+        }
+    }
+`;
+
+const SearchIcon = styled.div`
+    position: absolute;
+    top: 10px;
+    left: 3px;
+    width: 40px;
+    z-index: 1;
+    pointer-events: none;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    transition: background-color 0.15s;
+`;
+
+
 
 export default Header
